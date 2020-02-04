119ea39f55c8bfdc3547a69a628657f169bc5c3f
gwilliford@gmail.com <gwilliford@gmail.com>
2020-02-04 04:54:25 -0500

    Update analysis files

--- Analysis_Agreements_Logits_cyr.R
+++ Analysis_Agreements_Logits_cyr.R
@@ -1,9 +1,55 @@
+library(readr)
 library(brms)
 library(brglm)
 
+if(!exists("dat")) dat <- read_csv("./data/icow_part_cyr_ct.csv")
+
+
+a <- icow_full_cyr %>% filter((chal == 651 & tgt == 666) | (chal == 666 & tgt == 651))
+summary(a$year)
+
+
+### For some reason chal/tgt data has different results when using 1 and 2 and chal and tgt, and 1 and 2 differ from original file
+# Chal/tgt is actually dropping less observations
 # Dyadic: Unrealized gains from trade
-att_dy <- glm(attanyp ~ lugt + lntrade + laglntdepdy1 + laglntdepdy2 + laglntdeptot1 + laglntdeptot2 + 
-                   laglngdp1 + laglngdp2 + laglngdpcap1 + laglngdpcap2 + 
+
+att_dy <- glm(attanyp ~ lugt + lntrade + laglntdepdy1 + laglntdepdy2 + laglntdeptot1 + laglntdeptot2 + lugt * saltgt +
+                lag_1 + lag_2 + lag_cap1 + lag_cap2 + 
+                polity21 * polity22 +
+                icowsal + 
+                recnowt + recyeswt + recmidwt +
+                lcaprat + ldefense + contdir,
+              data = icow_part_cyr_ct,
+              family = binomial); summary(att_dy)
+
+att_dy <- brglm::brglm(attanyp ~ lrivugt + lntrade + laglntdepdy_chal + laglntdepdy_tgt + laglntdeptot_chal + laglntdeptot_tgt +
+                lag_ln_gdp_chal + lag_ln_gdp_chal + lag_ln_gdpcap_tgt + lag_ln_gdpcap_tgt + 
+                lpol_chal * lpol_tgt +
+                icowsal + 
+                recnowt + recyeswt + recmidwt +
+                lcaprat + ldefense + contdir,
+              data = icow_part_cyr_ct,
+              family = binomial); summary(att_dy)
+
+att_dy <- brglm::brglm(attanyp ~ ltotugt + laglntrade + laglntdepdy_chal + laglntdepdy_tgt + laglntdeptot_chal + laglntdeptot_tgt +
+                         lag_ln_gdp_chal + lag_ln_gdp_tgt + lag_ln_gdpcap_chal + lag_ln_gdpcap_tgt + 
+                         lpol_chal * lpol_tgt +
+                         icowsal + 
+                         recnowt + recyeswt + recmidwt +
+                         lcaprat + ldefense + contdir,
+                       data = icow_part_cyr_ct,
+                       family = binomial); summary(att_dy)
+att_dy <- MASS::glm.nb(attanyp ~ ltotugt + laglntrade + laglntdepdy_chal + laglntdepdy_tgt + laglntdeptot_chal + laglntdeptot_tgt +
+                         lag_ln_gdp_chal + lag_ln_gdp_tgt + lag_ln_gdpcap_chal + lag_ln_gdpcap_tgt + 
+                         lpol_chal * lpol_tgt +
+                         icowsal + 
+                         recnowt + recyeswt + recmidwt +
+                         lcaprat + ldefense + contdir,
+                       data = icow_part_cyr_ct); summary(att_dy)
+
+
+att_dy <- glm(attanyp ~ lugt + lntrade + laglntdepdy1 + laglntdepdy2 + laglntdeptot1 + laglntdeptot2 + lugt * salintc +
+                   lag_ln_gdp1 + lag_ln_gdp2 + lag_ln_gdpcap1 + lag_ln_gdpcap2 + 
                    polity21 * polity22 +
                    icowsal + 
                    recnowt + recyeswt + recmidwt +
@@ -12,7 +58,7 @@ att_dy <- glm(attanyp ~ lugt + lntrade + laglntdepdy1 + laglntdepdy2 + laglntdep
                  family = binomial); summary(att_dy)
 
 att_dy_b <- brm(attanyp ~ lugt + lntrade + laglntdepdy1 + laglntdepdy2 + laglntdeptot1 + laglntdeptot2 + 
-                         laglngdp1 + laglngdp2 + laglngdpcap1 + laglngdpcap2 + 
+                         lag_ln_gdp1 + lag_ln_gdp2 + lag_ln_gdpcap1 + lag_ln_gdpcap2 + 
                          polity21 * polity22 +
                          icowsal + 
                          recnowt + recyeswt + recmidwt +
@@ -23,7 +69,7 @@ att_dy_b <- brm(attanyp ~ lugt + lntrade + laglntdepdy1 + laglntdepdy2 + laglntd
 
 # Unrealized gains from trade dependence
 # att_dy <- brglm::brglm(attanyp ~ lugt + lntrade + laglntdepdy1 + laglntdepdy2 + laglntdeptot1 + laglntdeptot2 + 
-#                          laglngdp1 + laglngdp2 + laglngdpcap1 + laglngdpcap2 + 
+#                          lag_1 + lag_2 + lag_cap1 + lag_cap2 + 
 #                          polity21 * polity22 +
 #                          icowsal + 
 #                          recnowt + recyeswt + recmidwt +
@@ -33,7 +79,7 @@ att_dy_b <- brm(attanyp ~ lugt + lntrade + laglntdepdy1 + laglntdepdy2 + laglntd
 
 # Dyadic: Actual Trade
 att_dy <- glm(attanyp ~ lntrade + laglntdepdy1 + laglntdepdy2 + laglntdeptot1 + laglntdeptot2 + 
-                laglngdp1 + laglngdp2 + laglngdpcap1 + laglngdpcap2 + 
+                lag_1 + lag_2 + lag_cap1 + lag_cap2 + 
                 lpol1 * lpol2 +
                 icowsal + 
                 recnowt + recyeswt + recmidwt +
@@ -43,7 +89,7 @@ att_dy <- glm(attanyp ~ lntrade + laglntdepdy1 + laglntdepdy2 + laglntdeptot1 + 
 
 # Dyadic: Trade dependence
 att_dy <- glm(attanyp ~ lntrade + laglntdepdy1 + laglntdepdy2 + 
-                laglngdp1 + laglngdp2 + laglngdpcap1 + laglngdpcap2 + 
+                lag_1 + lag_2 + lag_cap1 + lag_cap2 + 
                 polity21 * polity22 +
                 icowsal + 
                 recnowt + recyeswt + recmidwt +
@@ -54,7 +100,7 @@ att_dy <- glm(attanyp ~ lntrade + laglntdepdy1 + laglntdepdy2 +
 
 # Systemic: Trade dependence
 att_dy <- glm(attanyp ~ lntrade + laglntdeptot1 + laglntdeptot2 + 
-                laglngdp1 + laglngdp2 + laglngdpcap1 + laglngdpcap2 + 
+                lag_1 + lag_2 + lag_cap1 + lag_cap2 + 
                 polity21 * polity22 +
                 icowsal + 
                 recnowt + recyeswt + recmidwt +
--- Survival.R
+++ Survival.R
@@ -1,23 +1,40 @@
 # Claim termination
 library(survival)
+library(brms)
 
 # Claim termination models
-a <- coxph(Surv(y0, year, chdrop) ~ laglntrade, dat = icow_part_cyr);  summary(a)
+a <- coxph(Surv(y0, year, chdrop) ~ ltotugt + laglntrade, dat = icow_part_cyr);  summary(a)
 a <- coxph(Surv(y0, year, tgtdrop) ~ laglntrade, dat = icow_part_cyr); summary(a)
-a <- coxph(Surv(y0, year, pset) ~ lugt + laglngdp1 + laglngdp2 + laglngdpcap1 + laglngdpcap2 + ldefense + contdir + lcaprat + polity21 * polity22, dat = icow_part_cyr); summary(a)
+a <- coxph(Surv(y0, year, pset) ~ lugt + lag_ln_gdp1 + lag_ln_gdp2 + lag_ln_gdpcap1 + lag_ln_gdpcap2 +
+             ldefense + contdir + lcaprat + lpol1 * lpol2, dat = icow_part_cyr); summary(a)
 
-a <- brm(chdrop ~ lugt + laglngdp1 + laglngdp2 + laglngdpcap1 + laglngdpcap2 + ldefense + contdir + lcaprat + polity21 * polity22 + year + I(year^2) + I(year^3) + (1|dyad),
-         family = "bernoulli",
-         dat = icow_part_cyr,
-         cores = 4); summary(a)
+
+sub <- na.omit(select(icow_part_cyr_ct, y0, year, chdrop, tgtdrop, pset, ltotugt, lterugt, lmarugt, lrivugt, starts_with("lag_ln_gdp"), ldefense, contdir, lcaprat, lpol_chal, lpol_tgt))
+a <- coxphf::coxphf(Surv(y0, year, chdrop) ~ ltotugt + lag_ln_gdp_chal + lag_ln_gdp_tgt + lag_ln_gdpcap1 + lag_ln_gdpcap2 + ldefense + contdir + lcaprat + lpol_chal * lpol_tgt, dat = sub); summary(a)
+a <- coxph(Surv(y0, year, tgtdrop) ~ ltotugt + lag_ln_gdp_chal + lag_ln_gdp_tgt + lag_ln_gdpcap1 + lag_ln_gdpcap2 + ldefense + contdir + lcaprat + lpol_chal * lpol_tgt + (1|dyad),
+           dat = icow_part_cyr_ct); summary(a)
+a <- coxph(Surv(y0, year, pset) ~ lrivugt + lag_ln_gdp_chal + lag_ln_gdp_tgt + lag_ln_gdpcap1 + lag_ln_gdpcap2 + ldefense + contdir + lcaprat + lpol_chal * lpol_tgt + (1|dyad),
+           dat = icow_part_cyr_ct); summary(a)
+
+a <- brm(pset ~ lrivugt + lag_ln_gdp_chal + lag_ln_gdp_tgt + lag_ln_gdpcap1 + lag_ln_gdpcap2 + ldefense + contdir + lcaprat + lpol_chal + lpol_tgt + year + y2 + y3 + (1|dyad),
+           dat = icow_part_cyr_ct,
+          family = "bernoulli",
+          cores = 4); summary(a)
+
+
+# a <- brm(chdrop ~ ltotugt + lag_lngdp1 + lag_ln_gdp2 + lag_ln_gdpcap1 + lag_ln_gdpcap2 + ldefense + contdir + lcaprat + polity21 * polity22 + year + I(year^2) + I(year^3) + (1|dyad),
+#          family = "bernoulli",
+#          dat = icow_part_cyr,
+#         #          family = "bernoulli",
+
 
 
 # Time to agreement models
 a <- coxph(Surv(y0, year, chdrop) ~ laglntrade, dat = icow_part_cyr);  summary(a)
-a <- coxph(Surv(y0, year, agree) ~ lugt + mlntrade + laglngdp1 + laglngdp2 + laglngdpcap1 + laglngdpcap2 + ldefense + contdir + lcaprat + polity21 * polity22, dat = icow_part_cyr);  summary(a)
+a <- coxph(Surv(y0, year, agree) ~ lugt + mlntrade + lag_ln_gdp1 + lag_ln_gdp2 + lag_ln_gdpcap1 + lag_ln_gdpcap2 + ldefense + contdir + lcaprat + polity21 * polity22, dat = icow_part_cyr);  summary(a)
 
-a <- coxph(Surv(y0, year, agree) ~ laglntrade + mlntrade + laglngdp1 + laglngdp2 + laglngdpcap1 + laglngdpcap2 + ldefense + contdir + lcaprat + polity21 * polity22, dat = icow_part_cyr);  summary(a)
-a <- coxph(Surv(y0, year, agreeiss) ~ laglntrade + mlntrade + laglngdp1 + laglngdp2 + laglngdpcap1 + laglngdpcap2 + ldefense + contdir + lcaprat + polity21 * polity22, dat = icow_part_cyr);  summary(a)
-a <- coxph(Surv(y0, year, ag_end_any) ~ laglntrade + mlntrade + laglngdp1 + laglngdp2 + laglngdpcap1 + laglngdpcap2 + ldefense + contdir + lcaprat + polity21 * polity22, dat = icow_part_cyr);  summary(a)
-a <- coxph(Surv(y0, year, ag_end_part) ~ laglntrade + mlntrade + laglngdp1 + laglngdp2 + laglngdpcap1 + laglngdpcap2 + ldefense + contdir + lcaprat + polity21 * polity22, dat = icow_part_cyr);  summary(a)
-a <- coxph(Surv(y0, year, ag_end_full) ~ laglntrade + mlntrade + laglngdp1 + laglngdp2 + laglngdpcap1 + laglngdpcap2 + ldefense + contdir + lcaprat + polity21 * polity22 + frailty(dyad), dat = icow_part_cyr);  summary(a)
+a <- coxph(Surv(y0, year, agree) ~ laglntrade + mlntrade + lag_ln_gdp1 + lag_ln_gdp2 + lag_ln_gdpcap1 + lag_ln_gdpcap2 + ldefense + contdir + lcaprat + polity21 * polity22, dat = icow_part_cyr);  summary(a)
+a <- coxph(Surv(y0, year, agreeiss) ~ laglntrade + mlntrade + lag_ln_gdp1 + lag_ln_gdp2 + lag_ln_gdpcap1 + lag_ln_gdpcap2 + ldefense + contdir + lcaprat + polity21 * polity22, dat = icow_part_cyr);  summary(a)
+a <- coxph(Surv(y0, year, ag_end_any) ~ laglntrade + mlntrade + lag_ln_gdp1 + lag_ln_gdp2 + lag_ln_gdpcap1 + lag_ln_gdpcap2 + ldefense + contdir + lcaprat + polity21 * polity22, dat = icow_part_cyr);  summary(a)
+a <- coxph(Surv(y0, year, ag_end_part) ~ laglntrade + mlntrade + lag_ln_gdp1 + lag_ln_gdp2 + lag_ln_gdpcap1 + lag_ln_gdpcap2 + ldefense + contdir + lcaprat + polity21 * polity22, dat = icow_part_cyr);  summary(a)
+a <- coxph(Surv(y0, year, ag_end_full) ~ laglntrade + mlntrade + lag_ln_gdp1 + lag_ln_gdp2 + lag_ln_gdpcap1 + lag_ln_gdpcap2 + ldefense + contdir + lcaprat + polity21 * polity22 + frailty(dyad), dat = icow_part_cyr);  summary(a)
