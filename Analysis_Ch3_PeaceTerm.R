d94c0b97994397fd93a1003fa183e51de4a79796
George Williford <31017813+gwilliford@users.noreply.github.com>
2020-11-20 21:54:48 -0600

    Update Analysis_Ch3_PeaceTerm.R
    Update models to use geometric mean. Add models for peace agreements and attempts.

--- Analysis_Ch3_PeaceTerm.R
+++ Analysis_Ch3_PeaceTerm.R
@@ -23,9 +23,14 @@ registerDoSNOW(cl)
 # Data Management
 ################################################################################
 
+peaceatt = read.dta13("C:/Users/gwill/Dropbox/Research/Dissertation/Data Management/outputdata/ICOWPeaceAttemptData.dta")
+peaceatt = rename(peaceatt, "st" = "_st", "event" = "_d", "stop" = "_t", "start" = "_t0")
+
+peaceagr = read.dta13("C:/Users/gwill/Dropbox/Research/Dissertation/Data Management/outputdata/ICOWPeaceAgreementData.dta")
+peaceagr = rename(peaceagr, "st" = "_st", "event" = "_d", "stop" = "_t", "start" = "_t0")
+
 peaceterm = read.dta13("C:/Users/gwill/Dropbox/Research/Dissertation/Data Management/outputdata/ICOWPeaceTerminationData.dta")
 peaceterm = rename(peaceterm, "st" = "_st", "event" = "_d", "stop" = "_t", "start" = "_t0")
-
 
 ################################################################################
 # Descriptive Statistics
@@ -47,22 +52,74 @@ abline(v = pterm_sdabove, lty = 3)
 abline(v = pterm_sdbelow, lty = 3)
 
 
+att_cox = coxph(Surv(start, stop, event) ~ ltradedep_geomean + lpchcap + caprat +
+                  icowsal + riveriss + mariss + 
+                  recmidwt + recnowt + recyeswt + bdymid + 
+                  demdy + autdy + contdir + defense + igosum,
+                data = peaceatt); summary(att_cox)
+att_coxphf = coxphf(Surv(start, stop, event) ~ ltradedep_geomean + lpchcap + caprat +
+                  icowsal + riveriss + mariss + 
+                  recmidwt + recnowt + recyeswt + bdymid + 
+                  demdy + autdy + contdir + defense + igosum,
+                data = peaceatt); summary(att_coxphf)
+
+att_cure = tvcure(Surv(start, stop, event) ~ lpchcap + recmidwt + recnowt + recyeswt + bdymid,
+            cureform = ~ ltradedep_geomean + caprat +
+              icowsal + riveriss + mariss +
+              demdy + autdy + contdir + defense + igosum, 
+            data = peaceatt, 
+            link = "probit",
+            brglm = T); summary(att_cure)
+
+agr_cox = coxph(Surv(start, stop, event) ~ ltradedep_geomean + lpchcap + caprat +
+                       icowsal + riveriss + mariss + 
+                       recmidwt + recnowt + recyeswt + bdymid + 
+                       demdy + autdy + contdir + defense + igosum,
+                     data = peaceatt); summary(agr_cox)
+
+agr_cure = tvcure(Surv(start, stop, event) ~ lpchcap + recmidwt + recnowt + recyeswt + bdymid,
+                  cureform = ~ ltradedep_geomean + caprat +
+                    icowsal + riveriss + mariss +
+                    demdy + autdy + contdir + defense + igosum, 
+                  data = peaceagr, 
+                  link = "probit",
+                  brglm = T); summary(agr_cure)
+
+set_cox = coxph(Surv(start, stop, event) ~ ltradedep_geomean + lpchcap + caprat +
+                  icowsal + riveriss + mariss + 
+                  recmidwt + recnowt + recyeswt + bdymid + 
+                  demdy + autdy + contdir + defense + igosum,
+                data = peaceterm); summary(set_cox)
+
+set_cure = tvcure(Surv(start, stop, event) ~ lpchcap + recmidwt + recnowt + recyeswt + bdymid,
+                  cureform = ~ ltradedep_geomean + caprat +
+                    icowsal + riveriss + mariss +
+                    demdy + autdy + contdir + defense + igosum, 
+                  data = peaceterm, 
+                  link = "probit",
+                  brglm = T); summary(set_cure)
+
+
 ################################################################################
 # Analysis
 ################################################################################
-m2 = coxph(Surv(start, stop, event) ~ ltradedepmin_logc + recmidwt + recnowt + icowsal, data = peaceterm); summary(m2)
-m2 = tvcure(Surv(start, stop, event) ~ ltradedepmin_logc + lpchcap + recmidwt + recnowt + recyeswt + bdymid,
-            cureform = ~ ltradedepmin_logc + caprat + icowsal + riveriss + mariss + demdy + autdy + contdir + defense + igosum, 
-            data = peaceterm, 
+m1 = coxph(Surv(start, stop, event) ~ ltradedep_geomean + lpchcap + caprat +
+             icowsal + riveriss + mariss + 
+             recmidwt + recnowt + recyeswt + bdymid + 
+             demdy + autdy + contdir + defense + igosum,
+           data = peaceterm); summary(m1)
+m2 = tvcure(Surv(start, stop, event) ~ ltradedep_geomean +
+              lpchcap + recmidwt + recnowt + recyeswt + bdymid,
+            cureform = ~ ltradedep_geomean + caprat + icowsal + riveriss + mariss + demdy + contdir + defense, 
+            data = peaceterm,
+            link = "probit",
             brglm = T); summary(m2)
-m2 = tvcure(Surv(start, stop, event) ~ lpchcap + recmidwt + recnowt + recyeswt + bdymid,
-            cureform = ~ ltradedepmin_logc + icowsal + riveriss + mariss + demdy + autdy + contdir + defense + igosum, 
-            data = peaceterm, 
-            brglm = T); summary(m2)
-m2 = tvcure(Surv(start, stop, event) ~ ltradedepmin_logc + lpchcap + recmidwt + recnowt + recyeswt + bdymid,
+m4 = tvcure(Surv(start, stop, event) ~ ltradedep_geomean + lpchcap + recmidwt + recnowt + recyeswt + bdymid,
             cureform = ~ icowsal + riveriss + mariss + demdy + autdy + contdir + defense + igosum, 
             data = peaceterm, 
-            brglm = F); summary(m2)
+            brglm = F); summary(m4)
+
+# Theory - public pushes policymakers to take action - leads to settlement attempts and settlements
 ################################################################################
 # Results of testing
 ################################################################################
@@ -93,6 +150,16 @@ m2 = tvcure(Surv(start, stop, event) ~ ltradedepmin_logc + lpchcap + recmidwt + 
   # null for hazard only
 
 # ltradedepmin_logc
-  # both hull
+  # both null
   # cure only null
   # hazard only null
+
+# ltradedepmin_logc1
+  # both null
+  # cure only null
+  # hazard only null
+
+# ltradedep_geomean
+  # both - significant at .05 in glm equation - separation
+  # glm only - could be significant at .01 level, separation
+  # hazard only - insignificant
