45bc09ab4460a63c65cc93e7c638a8438b7e0e6f
George Williford <31017813+gwilliford@users.noreply.github.com>
2021-03-20 01:52:07 -0500

    Move files to old analysis files

--- Ch3_Descriptives_CMbyClaim.R
+++
@@ -1,8 +0,0 @@
-# Caculates total number of attempts, agreements, and concessions by claim
-# Uses icow_set_cldy from claim management formatting file
-a = icow_set_cldy %>% group_by(claimdy) %>% summarize(
-  sbagree = max(bagree), 
-  sbagreeiss = max(bagreeiss),
-  sbagconc = max(bagconc)
-)
-summary(a)
--- Descriptives_Ch3_BorderSettlement.R
+++
@@ -1,7 +0,0 @@
-library(dplyr)
-ibad = read.csv("C:/Users/gwill/Dropbox/Research/Dissertation/Data Management/sourcedata/ibaddyr.csv")
-ibad = ibad %>%
-  group_by(dyad) %>%
-  mutate(maxyear = max(year))
-
-summary(ibad$settle[ibad$year == ibad$maxyear])
