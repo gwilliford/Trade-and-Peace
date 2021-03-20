0a4ddc193bd4e20c74f99bec275faa67d986d8da
George Williford <31017813+gwilliford@users.noreply.github.com>
2021-03-20 01:53:13 -0500

    Remove manuscript and old files

--- old analysis files/TradePred - Neural nets.R
+++
@@ -1,7 +0,0 @@
-#Neural Net
-# library(neuralnet)
-# nn = neuralnet(Placed~TKS+CSS,data=df, hidden=3,act.fct = "logistic",
-#              linear.output = FALSE)
-# library(neuralnet)
-# datnarm <- na.omit(dat)
-# nn = neuralnet(trade ~ gdpcomb + dyterrclaim + conttype, data = datnarm)
